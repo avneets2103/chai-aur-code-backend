bc3edb29692ddc140849de5ebfca72af6c5f425f
Avneet Singh <avneets2103@gmail.com>
2024-01-26 15:04:59 +0530

    V9

---
+++ src/app.js
@@ -0,0 +1,20 @@
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+
+const app = express();
+
+// cors configure
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+}));
+
+// data khi jgah se aaega to uske lie settings lgti hai
+// unko parse krne k lie
+app.use(express.json({limit: "16kb"}))
+app.use(express.urlencoded({limit: "16kb"}));
+app.use(express.static("public"))
+app.use(cookieParser());
+
+export {app};
--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
 import dotenv from 'dotenv'
 import { dbConnect } from './db/index.js';
+import { app } from './app.js';
 
 dotenv.config({
     path: './env'
 })
 
-dbConnect();
+dbConnect()
+.then(()=>{
+    app.listen(process.env.PORT || 8000, ()=>{
+        console.log(`Server started at ${process.env.PORT}`);
+    });
+})
+.catch((error)=>{
+    console.log("DB connect error");
+    throw error;
+});
